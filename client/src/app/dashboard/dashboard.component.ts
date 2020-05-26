3136d7f8764c642b9b6d56c949b9cfb38dd80cc1
Pragati <pragsjainprags@gmail.com>
2020-05-26 13:33:51 +0530

    dashboard features

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -11,12 +11,15 @@ import { ToastrService } from 'ngx-toastr';
 export class DashboardComponent implements OnInit {
   isGroupList=false;
   groupList=[];
+  yourShareArray=[];
+  currentUserId;
   constructor(private router: Router,
     private appService: AppService,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.getAllGroups()
+    this.currentUserId=this.appService.getUserInfoFromLocalstorage().userId;
   }
   
   getAllGroups(){
@@ -26,6 +29,10 @@ export class DashboardComponent implements OnInit {
       if(!res.error){
       this.isGroupList=true;
       this.groupList=res.data;
+      this.groupList.forEach(element => {
+        this.getAllExpensesforYourShare(element.groupId,element.groupName);
+      });
+      console.log(this.yourShareArray);
       }
       else{
         this.isGroupList=false;
@@ -50,6 +57,31 @@ export class DashboardComponent implements OnInit {
 
   editGroup(groupId){
     this.router.navigate(['/group',groupId]);
+  }
+
+  getAllExpensesforYourShare(groupId,groupName){
+    this.appService.getAllExpenses(groupId,0,0).subscribe( (res) =>{
+      if(!res.error){
+        console.log(res.data);
+        let netBalance=0;
+        res.data.expenseList.forEach(element => {
+          let currentUserId=element.expenseMembers.filter(el =>el.userId==this.currentUserId);
+          if(currentUserId.length>0)
+          netBalance=netBalance+(currentUserId['0'].paidShare-currentUserId['0'].owedShare)
+        });
+        
+        let text= `you ${netBalance>0?'get back':'owe'}`
+        let share= netBalance==0?`nothing`:Math.abs(netBalance).toFixed(2);
+        let yourShare=`${text} <b>Rs.${share}</b> for Group <b>${groupName}</b> which has <span style="color: #666;">${res.data.count} expense(s)</span>`;
+        console.log(yourShare);
+        this.yourShareArray.push(yourShare);
+      }
+      else{
+       this.toastr.error(res.message)
+      }
+    },(error)=>{
+      console.log('error',error);
+    })
   }
 
 }
